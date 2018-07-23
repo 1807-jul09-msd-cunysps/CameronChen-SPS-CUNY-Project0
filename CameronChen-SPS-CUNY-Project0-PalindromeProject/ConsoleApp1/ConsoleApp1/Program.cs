6ec096b6346b3eb02517bb44c77b6ef37a85efbe
cameron-chen43 <41022853+cameron-chen43@users.noreply.github.com>
2018-07-23 11:15:40 -0400

    palindrome project uploaded

---
+++ CameronChen-SPS-CUNY-Project0-PalindromeProject/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Palindrome2;
+
+namespace MainFunction
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string st = "A nut for a jar of tuna";
+            Palindrome var = new Palindrome();
+            bool check = var.IsPalindrome(st);
+
+            Console.Read();
+
+        }
+
+    }
+}
